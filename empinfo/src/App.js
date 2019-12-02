e1b6435bf20e864ad126e20f55afbea36a91f55f
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-02 13:23:40 +0530

    Changes

--- empinfo/src/App.js
+++ empinfo/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import MainCard from "./Components/Main";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="BackGround">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Darshan" Role="Systems analyst" />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Uday" Role="Technical support" />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Sai" Role="Network engineer" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Harshal" Role="Technical consultant" />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Hitendra" Role="Web developer" />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 CardBox">
+            <MainCard name="Shyam" Role="Business analyst" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
