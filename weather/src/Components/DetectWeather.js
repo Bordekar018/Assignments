64232eeb95951a18c7bffad7a8dc852b1d5ed9be
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-07 15:18:11 +0530

    Detect Weather

---
+++ weather/src/Components/DetectWeather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DetectWeather(latitude) {
+  return <div />;
+}
+
+export default DetectWeather;
--- weather/src/Components/latlong.js
+++ weather/src/Components/latlong.js
@@ -6,27 +6,45 @@ export class Weather extends Component {
 
     this.state = {
       Latitude: "",
-      Error: ""
+      error: ""
     };
-  }
-
-  render() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
+        console.log(position);
         this.setState({ Latitude: position.coords.latitude });
       },
       err => {
         console.log(err);
+        this.setState({ error: err.message });
       }
     );
-    return (
-      <div>
-        <h2>
-          Latitude:{this.state.Latitude}
-          {/* , Error:{this.state.Error} */}
-        </h2>
-      </div>
-    );
+  }
+
+  render() {
+    if (this.state.Latitude && !this.state.error) {
+      return (
+        <div>
+          <h2 className="neutro">
+            Latitude - {this.state.Latitude}
+          </h2>
+        </div>
+      );
+    }
+    if (!this.state.Latitude && this.state.error) {
+      return (
+        <div>
+          <h2 className="neutro">
+            Error - {this.state.error}
+          </h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className="neutro">
+          <h2>Loading.....</h2>
+        </div>
+      );
+    }
   }
 }
 
