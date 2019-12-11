0abd92161016a2ae704e4950ce8e87b7fc228304
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-11 11:45:19 +0530

    SetUp

--- youtubesearch/src/App.js
+++ youtubesearch/src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import searchIcon from "./Uploads/searchIcon.png";
+import "./App.css";
+import Player from "./Components/Player";
+import Playlist from "./Components/Playlist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 " />
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 ">
+          <div className="input-group mb-3 searchBox">
+            <input type="text" className="form-control" placeholder="Search" />
+            <div className="input-group-append SearchDiv">
+              <button
+                className="btn btn-outline-secondary btnSearch"
+                type="button"
+                id="button-addon2"
+              >
+                <img src={searchIcon} className="btnImg" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 " />
+      </div>
+      <div className="row other">
+        <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 ">
+          <Player />
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <Playlist />
+        </div>
+      </div>
     </div>
   );
 }
