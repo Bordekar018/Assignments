75967d4a7808754890baaf9a6cca61ec7f6b1d96
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-12 04:17:37 +0530

    chnage

--- youtubesearch/src/Components/PlaylistComponent/ParticularVideo.js
+++ youtubesearch/src/Components/PlaylistComponent/ParticularVideo.js
@@ -9,7 +9,7 @@ function ParticularVideo(props) {
         className="card-img-top"
         src={PassedDataToThisComponent.snippet.thumbnails.high.url}
       />
-      <div className="card-body">
+      <div className="card-body a">
         <h4 className="card-title">
           {PassedDataToThisComponent.snippet.title}
         </h4>
