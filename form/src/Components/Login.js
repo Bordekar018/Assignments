cc478a224e408bb7524008f9ed6eb62895420df6
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-15 01:32:15 +0530

    change

--- form/src/Components/Login.js
+++ form/src/Components/Login.js
@@ -1,6 +1,42 @@
 import React, { Component } from "react";
 
 export class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usernameLogin: "",
+      passwordLogin: ""
+    };
+  }
+  getUsername = e => {
+    this.setState({ usernameLogin: e.target.value });
+  };
+
+  getPassword = e => {
+    this.setState({ passwordLogin: e.target.value });
+  };
+
+  getDataFromLocalStorage = () => {
+    return localStorage.getItem("StoreInfo");
+  };
+
+  convertDatajsonIntoObject = () => {
+    let localStorageData = this.getDataFromLocalStorage();
+    return JSON.parse(localStorageData);
+  };
+
+  loginUser = () => {
+    let jsdata = this.convertDatajsonIntoObject();
+    console.log(jsdata);
+    //console.log(jsdata.firstName)
+    let uName = this.state.usernameLogin;
+    let uPass = this.state.passwordLogin;
+    if (uName == jsdata.userlogin.username && uPass == userlogin.password) {
+      alert(`${jsdata.firstName} ${jsdata.lastname}`);
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -15,6 +51,8 @@ export class Login extends Component {
             className="form-control"
             aria-label="Default"
             aria-describedby="inputGroup-sizing-default"
+            value={this.state.usernameLogin}
+            onChange={this.getUsername}
           />
         </div>
 
@@ -29,13 +67,15 @@ export class Login extends Component {
             className="form-control"
             aria-label="Default"
             aria-describedby="inputGroup-sizing-default"
+            value={this.state.passwordLogin}
+            onChange={this.getPassword}
           />
         </div>
 
         <button
           type="button"
           className="btn btn-outline-success"
-          onClick={this.alertinfo}
+          onClick={this.loginUser}
         >
           Login
         </button>
