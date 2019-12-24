215c9ae450a4e663491ff942b1219f35128a3e74
Bordekar018 <darshanbordekar1819@gmail.com>
2019-12-24 22:40:50 +0530

    90%

--- fetchuser/src/Components/UserList.js
+++ fetchuser/src/Components/UserList.js
@@ -4,28 +4,37 @@ import { FetchUserData } from "../redux/Action/action";
 export class UserList extends Component {
   constructor(props) {
     super(props);
-    //console.log(props);
   }
   componentDidMount() {
     this.props.FetchUserData(this.props.id);
   }
+  showAuthor = () => {
+    console.log(this.props.UserData);
+  };
   render() {
     if (!this.props.UserData) {
       return null;
     }
+
     return (
-      <div className="container">
-        <div className="row">
-          <h1>
-            {this.props.UserData.name}
-          </h1>
-        </div>
-      </div>
+      <React.Fragment>
+        <h1>
+          {this.props.UserData.name}
+        </h1>
+        <button
+          type="button"
+          className="btn btn-info"
+          onClick={this.showAuthor}
+        >
+          See Author
+        </button>
+      </React.Fragment>
     );
   }
 }
 const SendStateToProps = (state, ownProps) => {
-  //console.log(state, ownProps);
+  //console.log(state, ownProps)
+
   console.log(state.PostFetchList.PostList);
   console.log(state.UserFetchList.UsersList);
 
