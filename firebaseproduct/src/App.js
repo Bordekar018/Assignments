b9122a89fea10b23988c04e54303b40618e6e394
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-04 13:38:01 +0530

    change

--- firebaseproduct/src/App.js
+++ firebaseproduct/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Navigation from "./Components/Navigation";
 import AdminPanel from "./Components/AdminPanel";
 import LoginAdmin from "./Components/LoginAdmin";
 import CRUDProduct from "./Components/CRUDProduct";
 import HomeProduct from "./Components/HomeProduct";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <HomeProduct />
+    <div className="mainBox">
+      <Navigation />
+      <React.Fragment>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/login" component={LoginAdmin} />
+
+        <HomeProduct />
+      </React.Fragment>
     </div>
   );
 }
--- firebaseproduct/src/index.js
+++ firebaseproduct/src/index.js
@@ -6,9 +6,15 @@ import "./ComponentCSS/AdminPanel.css";
 import "./ComponentCSS/Login.css";
 import "./ComponentCSS/CRUDProduct.css";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
