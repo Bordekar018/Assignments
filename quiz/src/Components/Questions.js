ca6adb0a82adc0c066f7bc388eaa7a1ce0bbd619
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-06 19:03:00 +0530

    save

--- quiz/src/Components/Questions.js
+++ quiz/src/Components/Questions.js
@@ -1,49 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { SurveyAction } from "../Redux/Action/SurveyAction";
-export class Questions extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {};
-  }
+let Questions = props => {
+  useEffect(() => {
+    props.SurveyAction();
+  }, []);
+  return (
+    <div className="App container">
+      {props.Question.map(data =>
+        <React.Fragment key={data.id}>
+          <h1
+            className="mt-5"
+            style={{ color: "gray", backgroundColor: "White" }}
+          >
+            {data.title}
+          </h1>
+          <p className="mt-5" style={{ fontSize: "19px" }}>
+            {data.description}
+          </p>
+          <button
+            type="button"
+            className="btn btn-danger mt-5 btn-lg pl-4 pr-4 pt-2 pb-2"
+          >
+            Next
+          </button>
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
-  componentDidMount() {
-    this.props.SurveyAction();
-  }
-
-  render() {
-    if (!this.props.Question) {
-      return null;
-    }
-    return (
-      <div className="App container">
-        {this.props.Question.map(data =>
-          <React.Fragment key={data.id}>
-            <h1
-              className="mt-5"
-              style={{ color: "gray", backgroundColor: "White" }}
-            >
-              {data.title}
-            </h1>
-            <p className="mt-5" style={{ fontSize: "19px" }}>
-              {data.description}
-            </p>
-            <button
-              type="button"
-              className="btn btn-danger mt-5 btn-lg pl-4 pr-4 pt-2 pb-2"
-              onClick={this.nextQues}
-            >
-              Next
-            </button>
-          </React.Fragment>
-        )}
-      </div>
-    );
-  }
-}
 const mapStateToProps = state => {
-  console.log(state.SurveyQuestion.slice(0, 10));
+  let a = state.SurveyQuestion.slice(0, 10);
+  console.log(a[0]);
   return {
     Question: state.SurveyQuestion.slice(0, 10)
   };
