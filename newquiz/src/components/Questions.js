fa6aa2453a369e7421d6b1bc9eaa6836a4178020
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-07 22:56:37 +0530

    change

--- newquiz/src/components/Questions.js
+++ newquiz/src/components/Questions.js
@@ -3,39 +3,48 @@ import React, { Component } from "react";
 export class Questions extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      arrayToStoredata: 1
+    };
   }
-  next = (datakey, key) => {
-    if (key == datakey) {
-      return true;
-    } else {
-      return false;
-    }
+  next = () => {
+    this.setState({ arrayToStoredata: this.state.arrayToStoredata + 1 });
   };
   render() {
     console.log(this.props);
-
-    return (
-      <React.Fragment>
-        {this.props.QuestionData.map((data, key) =>
-          <React.Fragment key={data.id}>
+    return this.props.QuestionData.map((data, key) => {
+      if (this.state.arrayToStoredata == data.id) {
+        return (
+          <React.Fragment>
             <h1>
+              {console.log(typeof this.state.arrayToStoredata)}
               {data.title}
             </h1>
-            <p>
-              {data.description}
-            </p>
-            <button
-              onClick={() => {
-                this.next(data.id, key);
-              }}
-            >
-              Next
+            <button type="button" class="btn btn-primary" onClick={this.next}>
+              next
             </button>
-            <hr />
           </React.Fragment>
-        )}
-      </React.Fragment>
-    );
+        );
+      }
+    });
+    // return (
+    //   <React.Fragment>
+    //     {this.props.QuestionData.map((data, key) =>
+    //       <React.Fragment key={data.id}>
+    //         <h1>
+    //           {console.log(this.state.arrayToStoredata)}
+    //           {data.title}
+    //         </h1>
+    //         <p>
+    //           {data.description}
+    //         </p>
+    //         <button>Next</button>
+    //         <hr />
+    //       </React.Fragment>
+    //     )}
+    //   </React.Fragment>
+    // );
   }
 }
 
