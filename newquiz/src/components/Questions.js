2ad2771297e3b4294b5a75875e9c017e43d3e341
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-07 23:04:24 +0530

    change

--- newquiz/src/components/Questions.js
+++ newquiz/src/components/Questions.js
@@ -11,23 +11,34 @@ export class Questions extends Component {
   next = () => {
     this.setState({ arrayToStoredata: this.state.arrayToStoredata + 1 });
   };
+  submit = () => {
+    alert("Thank You");
+  };
   render() {
     console.log(this.props);
-    return this.props.QuestionData.map((data, key) => {
-      if (this.state.arrayToStoredata == data.id) {
-        return (
-          <React.Fragment>
-            <h1>
-              {console.log(typeof this.state.arrayToStoredata)}
-              {data.title}
-            </h1>
-            <button type="button" class="btn btn-primary" onClick={this.next}>
-              next
-            </button>
-          </React.Fragment>
-        );
-      }
-    });
+    if (this.state.arrayToStoredata < 11) {
+      return this.props.QuestionData.map(data => {
+        if (this.state.arrayToStoredata == data.id) {
+          return (
+            <React.Fragment>
+              <h1>
+                {console.log(typeof this.state.arrayToStoredata)}
+                {data.title}
+              </h1>
+              <button type="button" class="btn btn-primary" onClick={this.next}>
+                next
+              </button>
+            </React.Fragment>
+          );
+        }
+      });
+    } else {
+      return (
+        <button type="button" class="btn btn-primary" onClick={this.submit}>
+          Submit
+        </button>
+      );
+    }
     // return (
     //   <React.Fragment>
     //     {this.props.QuestionData.map((data, key) =>
